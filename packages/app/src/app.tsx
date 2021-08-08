9f5b860f660ecd6383b67a3a6b7eec3adbe9303e
Begbeder <begbed.work@gmail.com>
2021-08-08 12:45:18 +0300

    [App]: added button

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -1,8 +1,14 @@
 import type React from 'react';
-import { Main } from '@sample/components';
+import { Main, Button } from '@sample/components';
 
 export interface AppProps {
   text: string;
 }
 
-export const App: React.VFC<AppProps> = ({ text }) => <Main text={text} />;
+export const App: React.VFC<AppProps> = ({ text }) =>
+(
+  <div>
+    <Main text={text} />
+    <Button text='button text' />
+  </div>
+);
